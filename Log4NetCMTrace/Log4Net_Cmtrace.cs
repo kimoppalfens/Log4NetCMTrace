bdb196361b1fae03422989bd8d8ba8077857fea1
Kim Oppalfens <kim.oppalfens@hotmail.com>
2016-09-14 09:47:57 +0200

    Initial commit of a class to support log4net logging in cmtrace format

---
+++ Log4NetCMTrace/Log4Net_Cmtrace.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Layout.Pattern;
+using System.Management;
+
+namespace Log4Net_CMTrace
+{
+    public class NumericLevelPatternConverter : PatternLayoutConverter
+    {
+        protected override void Convert(TextWriter writer, log4net.Core.LoggingEvent loggingEvent)
+        {
+            int LoggingLevel = 1;
+            switch (loggingEvent.Level.Value)
+            {
+                case 10000: LoggingLevel = 4; break;
+                case 30000: LoggingLevel = 5; break;
+                case 40000: LoggingLevel = 6; break;
+                case 60000: LoggingLevel = 2; break;
+                case 70000: LoggingLevel = 3; break;
+                default: LoggingLevel = 1; break;
+
+            }
+            writer.Write(LoggingLevel);
+        }
+    }
+
+    public class CMClientLogFolderPatternConverter : log4net.Util.PatternConverter
+    {
+        protected override void Convert(System.IO.TextWriter writer, object state)
+        {
+            string CMClientLogFolder = string.Empty;
+            ManagementScope scope = new ManagementScope("\\\\.\\ROOT\\ccm\\Policy\\Machine\\ActualConfig");
+            ObjectQuery query = new ObjectQuery("SELECT LogDirectory FROM CCM_Logging_GlobalConfiguration");
+            ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher(scope, query);
+            ManagementObjectCollection managementObjectCollection = managementObjectSearcher.Get();
+            using (ManagementObjectCollection.ManagementObjectEnumerator enumerator = managementObjectCollection.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    ManagementObject managementObject = (ManagementObject)enumerator.Current;
+                    CMClientLogFolder = managementObject["LogDirectory"].ToString();
+                }
+            }
+            writer.Write(CMClientLogFolder);
+        }
+    }
+    public class CMAdminUILogFolderPatternConverter : log4net.Util.PatternConverter
+    {
+        override protected void Convert(System.IO.TextWriter writer, object state)
+        {
+            string CMAdminUILogFolder = string.Empty;
+            try
+            {
+                CMAdminUILogFolder = Environment.GetEnvironmentVariable("SMS_ADMIN_UI_PATH");
+            }
+            catch
+            {
+                CMAdminUILogFolder = CMAdminUILogFolder = Environment.GetEnvironmentVariable("TEMP");
+            }
+            writer.Write(CMAdminUILogFolder + "\\..\\..\\AdminUILog");
+        }
+    }
+}
