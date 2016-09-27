bf8f1b37f26062223e772a333fda6c597127cc6b
Kim Oppalfens <kim.oppalfens@hotmail.com>
2016-09-27 11:29:26 +0200

    Add dll to Github for use in blog reference

--- Log4NetCMTrace/Properties/AssemblyInfo.cs
+++ Log4NetCMTrace/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("93eda58b-856b-4efa-987a-b3e75a372d59")]
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
 
 // Version information for an assembly consists of the following four values:
 //
